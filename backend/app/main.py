b1d206534305bc819a13881875aecc1eaa62e505
Emirhan Sergen ÇERGEL <117258974+EmirhanSergen@users.noreply.github.com>
2025-06-11 01:32:41 +0300

    Add CORS support

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 from .routes import users
 from .routes import calls, applications
+from .config import settings
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[origin.strip() for origin in settings.allowed_origins.split(',')],
+    allow_credentials=True,
+    allow_methods=["*"] ,
+    allow_headers=["*"] ,
+)
 
 @app.get("/")
 async def root():
