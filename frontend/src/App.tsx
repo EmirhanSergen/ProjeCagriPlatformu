2ec6034262e39cf1b0c891f214c74ae15506d5f3
E <emirhansergen@gmail.com>
2025-06-11 00:10:17 +0300

    Start

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,9 +16,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold text-blue-600">Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
